f7a81de60bbf836ec77aaffbc260e2c1b5aff0e5
Matthijs van den Bos <matthijs@vandenbos.org>
2021-04-04 15:53:28 +0200

    Clarify usage of XPathExpressionDiscoverer

--- src/Discoverer/XPathExpressionDiscoverer.php
+++ src/Discoverer/XPathExpressionDiscoverer.php
@@ -2,6 +2,7 @@
 
 namespace VDB\Spider\Discoverer;
 
+use Exception;
 use VDB\Spider\Resource;
 
 /**
@@ -10,8 +11,36 @@ use VDB\Spider\Resource;
  */
 class XPathExpressionDiscoverer extends CrawlerDiscoverer
 {
+    /**
+     * Set the XPath selector to use.
+     *
+     * This selector should look for `a` elements so that the Discoverer can
+     * extract their `href` attribute for further crawling.
+     *
+     * @param string $selector
+     * @throws Exception
+     */
+    public function __construct(string $selector)
+    {
+        if (!self::endsWith($selector, "/a")) {
+            throw new Exception("Please end your selector with '/a': " .
+                "selectors should look for `a` elements " .
+                "so that the Discoverer can extract their `href` attribute for further crawling.");
+        }
+        parent::__construct($selector);
+    }
+
     protected function getFilteredCrawler(Resource $resource)
     {
         return $resource->getCrawler()->filterXPath($this->selector);
+    }
+
+    private static function endsWith($haystack, $needle): bool
+    {
+        $length = strlen($needle);
+        if (!$length) {
+            return true;
+        }
+        return substr($haystack, -$length) === $needle;
     }
 }
