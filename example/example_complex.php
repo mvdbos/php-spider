47becc9d63fa1af30d60270c976dec6d15721293
Matthijs van den Bos <matthijs@vandenbos.org>
2024-02-22 15:02:38 +0100

    Add Green Metrics Tool config

--- example/example_complex.php
+++ example/example_complex.php
@@ -28,7 +28,7 @@ $allowSubDomains = true;
 
 // Create spider
 $spider = new Spider($seed);
-$spider->getDownloader()->setDownloadLimit(10);
+$spider->getDownloader()->setDownloadLimit(1000);
 
 $statsHandler = new StatsHandler();
 $LogHandler = new LogHandler();
